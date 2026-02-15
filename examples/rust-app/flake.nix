b363a5cede9ce20a65e2116344925b41abc49f69
King Art <arthur@ischium.net>
2026-02-15 03:06:11 +0000

    feat(examples): add rust-app example

---
+++ examples/rust-app/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Example Rust project using nix-zero-build";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    nix-zero-build = {
+      url = "github:your-org/nix-zero-build";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachSystem (import inputs.systems) (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "rust-app";
+          version = "0.1.0";
+          src = ./.;
+          # In a real project, this would be a hash or a generated file
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        packages.build-container = (inputs.nix-zero-build.lib pkgs).mkBuildContainer {
+          name = "rust-build-env";
+          contents = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            clippy
+          ];
+        };
+      }
+    );
+}
---
+++ examples/rust-app/src/main.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    text: String,
+    author: String,
+}
+
+fn main() {
+    let msg = Message {
+        text: String::from("Nix Zero Build"),
+        author: String::from("King Art"),
+    };
+
+    let j = serde_json::to_string(&msg).unwrap();
+    println!("Serialized: {}", j);
+}
